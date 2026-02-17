a5e2c2c68449f0e8e48d622f2cb5b59fae1bd171
lawaloy <lawaloy@users.noreply.github.com>
2026-02-16 21:41:22 -0600

    fix: patch vulnerable deps and stabilize unit specs

--- src/sms/sms.service.spec.ts
+++ src/sms/sms.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SmsService } from './sms.service';
+import { TwilioClient } from './twilio.client';
 
 describe('SmsService', () => {
   let service: SmsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SmsService],
+      providers: [
+        SmsService,
+        {
+          provide: TwilioClient,
+          useValue: {
+            sendMessage: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<SmsService>(SmsService);
