cbd128dfd5ea5d99329b251da0f9bb7b88beb44a
lawaloy <lawaloy@users.noreply.github.com>
2026-04-07 00:31:03 -0500

    chore: upgrade typescript and eslint tooling

--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -1,8 +1,24 @@
+const { fixupPluginRules } = require('@eslint/compat');
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
+const patchedExpoConfig = (Array.isArray(expoConfig) ? expoConfig : [expoConfig]).map((config) => {
+  if (!config.plugins) {
+    return config;
+  }
+
+  const patchedPlugins = Object.fromEntries(
+    Object.entries(config.plugins).map(([name, plugin]) => [name, fixupPluginRules(plugin)]),
+  );
+
+  return {
+    ...config,
+    plugins: patchedPlugins,
+  };
+});
+
 module.exports = defineConfig([
-  expoConfig,
+  ...patchedExpoConfig,
   {
     ignores: ['dist/**'],
   },
